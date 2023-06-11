24b705f85804f4af7cb0418b977f7711cca1e3e0
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-06-11 14:19:56 +0300

    Рефакторинг

--- src/main/java/pro/gorodnyuk/certificategenerator/CertificateGeneratorApplication.java
+++ src/main/java/pro/gorodnyuk/certificategenerator/CertificateGeneratorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CertificateGeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CertificateGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CertificateGeneratorApplication.class, args);
+    }
 
 }
